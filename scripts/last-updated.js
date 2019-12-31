a0452f0636c2792d08f03c3e38163da221751047
trnglina <contact@trnglina.org>
2019-12-31 09:58:02 -0700

    Various changes

--- scripts/last-updated.js
+++ scripts/last-updated.js
@@ -15,30 +15,38 @@
   const LastUpdatedIndicator = function(date) {
     const template = document.createElement('div');
     template.innerHTML = `
-        <sup class="last-updated">(up. ${date.replace(/-/g, '/')})</sup>`.trim();
+        <span class="last-updated">
+          <span class="marker">
+            up. <time datetime="${date}">${date.replace(/-/g, '/').substring(2)}</time>
+          </span>
+        </span>`.trim();
     return template.firstChild;
   };
 
   const makeRequest = function(linkObject, callback) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    const x = new XMLHttpRequest();
+    x.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         callback(linkObject, this);
       }
     };
-    xhttp.open('GET', linkObject.href, true);
-    xhttp.send();
+    x.open('GET', linkObject.href, true);
+    x.send();
   };
 
   const links = document.getElementsByClassName('update-date');
   for (let i = 0; i < links.length; i++) {
-    makeRequest(links[i], function(linkObject, xhttp) {
+    makeRequest(links[i], function(linkObject, x) {
       const dummy = document.createElement('html');
-      dummy.innerHTML = xhttp.response;
+      dummy.innerHTML = x.response;
       const metaElements = dummy.getElementsByTagName('meta');
       for (let j = 0; j < metaElements.length; j++) {
         if (metaElements[j].attributes['http-equiv'] && metaElements[j].attributes['http-equiv'].value == 'last-modified') {
-          linkObject.appendChild(new LastUpdatedIndicator(metaElements[j].attributes['content'].value));
+          const date = metaElements[j].attributes['content'].value;
+          const wrapper = new LastUpdatedIndicator(date);
+          linkObject.parentNode.insertBefore(wrapper, linkObject.nextSibling);
+          wrapper.insertBefore(linkObject, wrapper.firstChild);
+          wrapper.title = `Last updated ${date.replace(/-/g, '/')}`;
         }
       }
     });
